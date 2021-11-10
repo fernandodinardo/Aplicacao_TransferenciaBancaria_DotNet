f93691d8eb4c4a87672e45cc5ac680c3841be2b6
fernandodinardo <dinardo.lazarin@gmail.com>
2021-11-10 11:45:10 -0300

    Projeto de Transferência Bancária desenvolvido em CSharp / Orientado a Objetos / Enums

---
+++ Dio.Bank/Classes/Conta.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace Dio.Bank
+{
+    public class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+
+        private double Saldo { get; set; }
+        
+        private double Credito { get; set; }
+        
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            //Validação para saldo insulficiente
+            if (this.Saldo - valorSaque < (this.Credito *-1))
+            {
+                Console.WriteLine("Saldo insulficiente!");
+                return false;
+            }
+
+            this.Saldo -= valorSaque;
+
+            Console.WriteLine("O saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+
+            Console.WriteLine("O saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (this.Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Tipo_Conta " + this.TipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "Saldo " + this.Saldo + " | ";
+            retorno += "Credito " + this.Credito;
+            return retorno;
+        }
+    }
+}
